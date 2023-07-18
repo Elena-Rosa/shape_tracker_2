86efb0bb1bbff169a284ee56dd1e3a8737924be6
Elena <elenarosamail@gmail.com>
2023-07-17 18:28:52 -0700

    add configurations

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-function handleTriangleForm() {
+import Triangle from "./triangle.js";
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+function handleFormSubmission() {
   event.preventDefault();
-  document.querySelector('#response').innerText = null;
-  const length2 = parseInt(document.querySelector('#length2').value);
+  debugger;
   const length1 = parseInt(document.querySelector('#length1').value);
+  const length2 = parseInt(document.querySelector('#length2').value);
   const length3 = parseInt(document.querySelector('#length3').value);
   const triangle = new Triangle(length1, length2, length3);
   const response = triangle.checkType();
@@ -12,5 +17,5 @@ function handleTriangleForm() {
 }
 
 window.addEventListener("load", function () {
-  document.querySelector("#triangle-checker-form").addEventListener("submit", handleTriangleForm);
+  document.querySelector("#triangle-checker-form").addEventListener("submit", handleFormSubmission);
 });
--- src/triangle.js
+++ src/triangle.js
@@ -1,4 +1,4 @@
-function Triangle(side1, side2, side3) {
+export default function Triangle(side1, side2, side3) {
   this.side1 = side1;
   this.side2 = side2;
   this.side3 = side3;
